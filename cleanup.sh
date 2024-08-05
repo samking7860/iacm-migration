6d7369b748f2799abed1b1bf1eb92a57692b7051
David <david.warren@harness.io>
2024-08-05 15:44:23 +0100

    initial commit

---
+++ cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf .terraform terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl out/state out/.terraform out/terraform.tfstate out/terraform.tfstate.backup out/.terraform.lock.hcl
